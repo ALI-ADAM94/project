794a3f623c4f98d53a12f4378cab091fd3b35335
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-15 18:32:01 +0100

    Notification update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Chat from "./components/chat/Chat"
 import List from "./components/list/List"
 import Detail from './components/detail/Detail.jsx'
 import Login from "./components/login/Login";
+ import Notification from './components/notification/Notification';
 const App = () => {
   const user =false;
   return (
@@ -17,7 +18,7 @@ const App = () => {
 
     <Login/>
     )}
- 
+     <Notification/>
     </div>
   )
 }
