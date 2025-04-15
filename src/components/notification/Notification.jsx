b7dd907711d9fa1cc26aae1efc8a00c006910f0b
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-15 18:28:49 +0100

    Notification

---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,12 @@
+import { ToastContainer } from "react-toastify"
+
+
+const Notification = () => {
+  return (
+    <div>
+        <ToastContainer position="bottom-right"/>
+    </div>
+  )
+}
+
+export default Notification
