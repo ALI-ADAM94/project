c5faaf96e48501a277f16ca51024da54ffac2912
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-15 22:46:18 +0100

    styles

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -12,7 +12,7 @@ const AddUser = () => {
             <img src="./avatar.png" alt=""/>
             <span>Ali Adam</span>
         </div>
-         <button>AddUser</button>
+        <button>AddUser</button>
     </div>
     </div>
   )
